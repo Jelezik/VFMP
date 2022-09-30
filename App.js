e0aaf97a2c2a709115b78a8b31c0c12acd60048a
vlad <nekita.avdoshin@gmail.com>
2022-09-30 18:23:22 +0300

    add auth

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, Linking, StyleSheet, Text, View} from 'react-native';
+import {useCallback} from "react";
 
 export default function App() {
+
+  const urlAuth = 'https://oauth.vk.com/authorize?client_id=51438283&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=audio&response_type=token&v=5.194&state=123456';
+
+  const OpenURLButton = ({ url, children }) => {
+    const handlePress = useCallback(async () => {
+      const supported = await Linking.canOpenURL(url);
+
+      if (supported) {
+        await Linking.openURL(url);
+      } else {
+        Alert.alert(`Don't know how to open this URL: ${url}`);
+      }
+    }, [url]);
+
+    return <Button title={children} onPress={handlePress} />;
+  };
+
+
   return (
     <View style={styles.container}>
       <Text>Авторизируйтесь</Text>
-      <Button title="zhopa"/>
+      <OpenURLButton url={urlAuth}>Authorize</OpenURLButton>
     </View>
   );
 }
